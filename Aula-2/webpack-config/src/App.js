fe24533e86802273db49c2f83ccd8d1c2415397f
Marco Dittrichi <87870749+dittrichi@users.noreply.github.com>
2021-12-03 14:27:05 +0100

    Initial Create

---
+++ Aula-2/webpack-config/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Button from './Button'
+import Card from './Card'
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Bem vindo a nossa aula =D.</p>
+      <div>Webpack 4</div>
+      <Button />
+      <div><Card /></div>      
+    </div>
+  );
+};
+export default App;
